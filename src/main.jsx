097e01a934640694e1effd690c42780d152b7dd1
yasith <yasithruchiranga95@gmail.com>
2025-08-25 04:55:34 +0530

    `Update import from "react-router" to "react-router-dom" in main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {store} from'./lib/store';
 import { Provider } from 'react-redux';
 import { ClerkProvider } from '@clerk/clerk-react'
@@ -53,7 +53,7 @@ createRoot(document.getElementById('root')).render(
             <Route path="shop" element={<ShopPage />} /> 
 
           </Route>
-          <Route pathe="/">
+          <Route path="/">
             <Route path="About" element={<AboutUs />} />
             <Route path="Contact" element={<Contact />} />
             <Route path="Gallery" element={<Gallery />} /> {/* Gallery page */}
