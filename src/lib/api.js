08962f68144248e4c66d5e9cb053c75cc2726324
yasith <yasithruchiranga95@gmail.com>
2025-08-25 16:13:08 +0530

    `fix: whitespace change in import.meta.env.VITE_API_URL`

--- src/lib/api.js
+++ src/lib/api.js
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_API_URL,
+  baseUrl:import.meta.env.VITE_API_URL,
   prepareHeaders: async (headers) => {
     try {
       const clerk = window.Clerk;
